38e1cf9c156639dc9043d69242441abc00e099a2
Optimuspime123 <94816671+Optimuspime123@users.noreply.github.com>
2022-12-26 12:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ bot.hears('good morning', (ctx) => ctx.reply('Good morning to you too!'));
 bot.hears('Thomas', (ctx) => ctx.reply('Mopitibat has read your message, you fool! Stop annoying him'));
 
 bot.hears('Fela', (ctx) => ctx.reply('Fela is a great guy , love him! '));
-bot.hears('SoSo', (ctx) => ctx.reply('Careful , that's my son you're talking about  '));
+bot.hears('SoSo', (ctx) => ctx.reply('Careful, that is my son you are talking about.'));
+
 bot.hears('Optimus', (ctx) => ctx.reply('Aye aye captain! '));
 bot.launch();
